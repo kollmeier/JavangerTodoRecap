1fff85a3613849a0b6bf9e2e62dc87fb3d505fb8
Carsten Kollmeier <carsten@ckollmeier.de>
2025-04-23 10:27:39 +0200

    update code style

--- src/main/java/ckollmeier/de/javangertodorecap/JavangerTodoRecapApplication.java
+++ src/main/java/ckollmeier/de/javangertodorecap/JavangerTodoRecapApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavangerTodoRecapApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(JavangerTodoRecapApplication.class, args);
     }
 
