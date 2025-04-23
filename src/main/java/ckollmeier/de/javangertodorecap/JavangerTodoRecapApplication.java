e5ce8b8371a0312953b88b4d26ce2298459ea55b
Carsten Kollmeier <carsten@ckollmeier.de>
2025-04-23 10:24:52 +0200

    basic application

---
+++ src/main/java/ckollmeier/de/javangertodorecap/JavangerTodoRecapApplication.java
@@ -0,0 +1,13 @@
+package ckollmeier.de.javangertodorecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavangerTodoRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavangerTodoRecapApplication.class, args);
+    }
+
+}
