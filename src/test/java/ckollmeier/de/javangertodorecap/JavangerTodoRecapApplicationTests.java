e60e2aa842a204b42b1a33172f6050efc43fd3eb
Carsten Kollmeier <carsten@ckollmeier.de>
2025-04-23 10:21:41 +0200

    Initial commit

---
+++ src/test/java/ckollmeier/de/javangertodorecap/JavangerTodoRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package ckollmeier.de.javangertodorecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavangerTodoRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
